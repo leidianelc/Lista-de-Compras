93db873bdb6bb153d8011f0fc15fcfe5828a7d21
leidianelc <lei_dianecatty@hotmail.com>
2025-12-16 16:15:28 -0300

    melhora no codigo service

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,19 @@
-self.addEventListener("install",e=>{
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("lista-v1").then(c=>c.addAll([
-      "./","./index.html","./style.css","./script.js"
+    caches.open("lista-v1").then(c => c.addAll([
+      "./",
+      "./index.html",
+      "./style.css",
+      "./script.js",
+      "./manifest.json",
+      "./icon-192.png",
+      "./icon-512.png"
     ]))
   );
 });
-self.addEventListener("fetch",e=>{
+
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
